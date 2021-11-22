a7f5d9434138dfaa30b44b193b7e9912c5fdbf00
Silence-dream <1428482231@qq.com>
2021-11-22 18:01:00 +0800

    居家好物图片懒加载

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,12 +1,13 @@
 import XtxSkeleton from "@/components/library/XtxSkeleton";
 import XtxCarousel from "@/components/library/XtxCarousel";
 import XtxMore from "@/components/library/XtxMore";
-
+import lazy from "@/components/directive/lazy";
 const library = {
   install(app) {
     app.component(XtxSkeleton.name, XtxSkeleton);
     app.component(XtxCarousel.name, XtxCarousel);
     app.component(XtxMore.name, XtxMore);
+    app.directive("lazy", lazy);
   },
 };
 
